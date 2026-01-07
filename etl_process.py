38fe845d1a918f4fd648d095c624506746f169c9
Leroy Dsouza <lerdsouza@gmail.com>
2026-01-07 09:48:00 +0530

    Added risk logic using Numpy

--- etl_process.py
+++ etl_process.py
@@ -1,16 +1,18 @@
 import pandas as pd
-print("----Starting ETL Process----")
+import numpy as np
+print("----Starting ETL process----")
 df = pd.read_csv("trade_data.csv")
-print("Raw Data Loaded.", len(df))
+print("Raw data loaded", len(df))
 
 df = df.dropna(subset=['Ticker'])
 
-df['Quantity'] = df['Quantity'].fillna(0)
+df = df.fillna(0)
 
 df = df.drop_duplicates()
 
-df['Tradevalue'] = df['Quantity'] * df['Price']
+df['trade_value'] = df['Quantity'] * df['Price']
 
-df.to_csv("clean_trades.csv", index=False)
-print("Data cleaned and saved to 'clean_trades.csv'")
-print(df)
+df['Risk_Level'] = np.where(df['trade_value'] > 5000, 'High', 'Low')
+print("Risk Classification complete")
+
+df.to_csv('clean_trades.csv')
